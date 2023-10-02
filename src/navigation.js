ea42a6231c5212cb742012ee730b8337535c22ba
cam-alvarez <Calvarez.accts@gmail.com>
2023-10-02 11:21:30 -0400

    add scroll to top button to home page, blog landing page, and blog index file

--- src/navigation.js
+++ src/navigation.js
@@ -178,6 +178,6 @@ export const footerData = {
   // ],
   footNote: `
     <span class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm bg-"></span>
-    Made by  Camila Alvarez · All rights reserved · 2023 
+    Made by  Camila Alvarez ❤️ All rights reserved · 2023 
   `,
 };
